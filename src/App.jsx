02dcedec48d91ad8c5ef9ef796585d54c48296bb
Grégoire Aubertin <greg_abrn@icloud.com>
2025-06-25 11:29:40 +0200

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import { useState } from "react";
+import logoHappyCow from "./img/happycow-logo.svg";
+import { IoMdSearch } from "react-icons/io";
 import "./App.css";
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  return (
+    <>
+      <header>
+        <nav>
+          <div>
+            <img src={logoHappyCow} alt="Logo Happy Cow" />
+            <span>Explore</span>
+            <span>Forum</span>
+            <span>Blog</span>
+            <span>The App</span>
+            <span>Shop</span>
+            <span>More</span>
+          </div>
+          <div>
+            <IoMdSearch />
+            <button>Add Listing</button>
+            <button>Login / Join</button>
+          </div>
+        </nav>
+        <div></div>
+      </header>
+    </>
+  );
+};
 
 export default App;
